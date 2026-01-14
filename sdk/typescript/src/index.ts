d8bd109f95938df8b7ba6a63bfda1bfe02070af4
paritydotcx <paritydotcx@users.noreply.github.com>
2026-01-14 15:30:00 +0900

    add barrel exports for typescript sdk

---
+++ sdk/typescript/src/index.ts
@@ -0,0 +1,8 @@
+export { ParityClient } from "./client";
+export { SkillsApi } from "./skills";
+export { ContextApi } from "./context";
+export { AnalysisEngine } from "./analysis";
+export { SolanaProvider } from "./solana";
+export { SkillParser } from "./parser";
+export * from "./types";
+export * from "./constants";
